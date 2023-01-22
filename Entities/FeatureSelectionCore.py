2f1db016ed6beacc3b7d194634759d36284a8e78
Grigory Vasilev <grishavasilev62@gmail.com>
2023-01-22 20:39:27 +0300

    prepare to add unittest

--- Entities/FeatureSelectionCore.py
+++ Entities/FeatureSelectionCore.py
@@ -1,5 +1,10 @@
 from Entities.IFeatureSelectionCore import IFeatureSelectionCore
+import pandas as pd
 
 
 class FeatureSelectionCore(IFeatureSelectionCore):
+    def receive_dataset_file(self, file):
+        if (file):
+            self.__file = file
+
     pass
